15c37ab067faa1f011f5d5ad8d0dee4d22b54548
mayuresh-c19 <mayuresh1918@gmail.com>
2024-07-17 21:34:38 +0530

    Added MiddleWare and ErrorHandler

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,49 +1,50 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req,res) =>{
-    res.status(200).json({message : "Get All Contacts"});
-};
+const getContacts = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get All Contacts" });
+});
 
 //@desc get Individual contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req,res) =>{
-    res.status(200).json({message : `Get Contact for ${req.params.id}`});
-}
+const getContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+});
 
 //@desc Create contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req,res) =>{
-    console.log("The Request Body is : " ,req.body);
-    const {name,email,phone} = req.body;
-    if(!name || !email || !phone){
-        res.status(400);
-        throw new Error("All Fields are Madatory"); 
-    }
-    res.status(201).json({message : "Create Contact"});
-};
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The Request Body is : ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All Fields are Madatory");
+  }
+  res.status(201).json({ message: "Create Contact" });
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req,res) =>{
-    res.status(200).json({message : `Update Contact for ${req.params.id}`});
-};
-
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+});
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = (req,res) =>{
-    res.status(200).json({message : `Delete Contact for ${req.params.id}`})
-}
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
+});
 
 module.exports = {
-    getContacts,
-    createContact,
-    getContact,
-    updateContact,
-    deleteContact
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
 };
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5001;
 const app = express();
+const errorHandler = require("./middleware/errorHandler");
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 // If I have to send response ar Json
 // app.get("/api/contacts", (req,res) =>{
