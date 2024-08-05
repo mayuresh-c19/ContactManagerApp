0ada603a3de1e6c31db05d3f4f9debe61265fb7f
mayuresh-c19 <mayuresh1918@gmail.com>
2024-08-05 16:36:40 +0530

    User Login Functionality Done ✅

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel")
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 
 //@desc Register a user
@@ -23,6 +24,7 @@ const registerUser = asyncHandler(async (req,res) =>{
   const hashedPassword = await bcrypt.hash(password,10);
   console.log("Hashed Password : ",hashedPassword);
 
+  // Create user or put user data in MongoDB
   const user = await User.create({
     username,
     email,
@@ -46,7 +48,33 @@ const registerUser = asyncHandler(async (req,res) =>{
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req,res) =>{
-  res.status(200).json({message : "User Logged SuccessFully"});
+  const {email,password} =req.body;
+
+  if(!email || !password){
+    res.status(400);
+    throw new Error("All Fields are Mandatory");
+  }
+
+  const user = await User.findOne({email});
+
+  // Compare the Passwords
+  if(user && (await bcrypt.compare(password,user.password))){
+    const accessToken = jwt.sign(
+      {
+        user:{
+          username : user.username,
+          email : user.email,
+          id : user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {expiresIn : "1m"}
+    );
+    res.status(200).json({message : "User Login SuccessFull"});
+  }else{
+    res.status(401);
+    throw new Error("Email or Password is not valid");
+  }
 })
 
 //@desc Login a user
